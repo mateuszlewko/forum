12e79e7fa423761dca778e16d5cd5d258fb64153
Mateusz Lewko <mateusz.lewko@gmail.com>
2017-01-24 03:05:04 +0100

    Client almost done

--- forum-client/forum-client/forum-client.fs
+++ forum-client/forum-client/forum-client.fs
@@ -10,47 +10,60 @@ let write (stream : NetworkStream) (text : string) =
     let input = Encoding.UTF8.GetBytes text
 
     if stream.CanWrite then
-        stream.Write(input, 0, input.Length)
+        stream.Write (input, 0, input.Length)
+        Thread.Sleep 1
     else
-        printfn "Can't write"
+        printfn "Can't write stream"
 
 let sendLine stream = write stream <| Console.ReadLine()
+
+let commandsInfo () = 
+    printfn "--- Commands ---\n1 - login, 2 - register, 3 - logout, 4 - get threads,\n5 - get #n posts, 6 - new thread, 7 - new post,\n0 | cmds - print this info\n--- * ---"
 
 let parseAction (stream : NetworkStream) = 
     function
     | "1" | "l" -> 
         write stream LOGIN
-        printfn "--- Login ---\nname: "
+        printf "--- Login ---\nname: "
         sendLine stream
-        printfn "pass: "
+        printf "pass: "
         sendLine stream
         write stream CommandEnd
     | "2" | "r" -> 
-        ()
+        write stream REGISTER
+        printf "--- Register ---\nnew name: "
+        sendLine stream
+        printf "pass: "
+        sendLine stream
+        write stream CommandEnd
     | "3" | "out" ->
-        ()
+        write stream LOGOUT
+        write stream CommandEnd
     | "4" | "ths" ->
-        ()
+        printfn "--- Threads --- "
+        write stream GET_THREADS
+        write stream CommandEnd
     | "5" | "psts" ->
         ()
     | "6" | "nthr" ->
-        ()
+        write stream ADD_THREAD
+        printf "--- New threads ---\nTitle: "
+        sendLine stream
+        write stream CommandEnd
     | "7" | "npst" ->
         ()
+    | "cmds" -> commandsInfo ()
     | cmd -> printfn "Unknown command: %s" cmd
 
 
 let rec asyncSendInput (stream : NetworkStream) =
     async {
-        let input = 
-            Console.ReadLine ()
-            |> Encoding.UTF8.GetBytes
-
         if stream.CanWrite then
-            stream.Write(input, 0, input.Length)
+            Console.ReadLine ()
+            |> parseAction stream
+
+            // commandsInfo ()
             return! asyncSendInput stream
-        else
-            printfn "Can't write"
     }
 
 let read (stream : NetworkStream) =
@@ -67,7 +80,7 @@ let rec asyncPrintResponse (stream : NetworkStream) =
             printfn "%s" <| read stream
             return! asyncPrintResponse stream
         else
-            printfn "Can't read."
+            printfn "Can't read stream."
     }
 
 [<EntryPoint>]
@@ -75,6 +88,7 @@ let main args =
     let client = new System.Net.Sockets.TcpClient()
     client.Connect(args.[0], Int32.Parse(args.[1]))
     printfn "Connected to %A %A..." args.[0] args.[1]
+    commandsInfo ()
     printfn "\nType number corresponding to action."
     let stream = client.GetStream()
 
